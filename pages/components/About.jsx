811a8144bfb6a785c334c98d707b1ac8fd24a9b7
itsmenikhill <nklsharma2152@gmail.com>
2022-11-18 23:59:21 +0530

    added dark mode properties

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -1,21 +1,42 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div className="items-center md:justify-start md:ml-[25%] ">
+    <motion.div
+      className="items-center md:justify-start md:ml-[25%]"
+      // transition={{ delay: 0.5 }}
+    >
       <div className="md:p-20 p-8">
-        <h1 className="text-white font-semibold text-7xl md:text-9xl tracking-wider">
-          NIK<span className="text-cyan-500">HI</span>L SHARMA
-        </h1>
-        <h2 className="text-white font-semibold text-4xl md:text-7xl tracking-wider pt-12 pb-4">Software Dev</h2>
-        <p className="text-lg pt-8 text-white tracking-wider max-w-3xl pb-20">
+        <motion.h1
+          className="text-gray-700 dark:text-white font-semibold text-7xl md:text-9xl tracking-wider"
+          initial={{ y: 1000, opacity: 0, color: "white" }}
+          animate={{ y: 0, opacity: 1, color: "#374151" }}
+          transition={{ duration: 0.6, bounce: 0 }}
+        >
+          NIKHIL SHARMA
+        </motion.h1>
+        <motion.h2
+          className="text-gray-700 dark:text-white font-semibold text-4xl md:text-7xl tracking-wider pt-12 pb-4"
+          initial={{ y: 1000, opacity: 0, color: "white" }}
+          animate={{ y: 0, opacity: 1, color: "#374151" }}
+          transition={{ duration: 0.6, bounce: 0, delay: 0.60 }}
+        >
+          Software Dev
+        </motion.h2>
+        <motion.p
+          className="text-lg pt-8 text-gray-700 dark:text-white tracking-wider max-w-3xl pb-20"
+          initial={{ y: 1000, opacity: 0, color: "white" }}
+          animate={{ y: 0, opacity: 1, color: "#374151" }}
+          transition={{ duration: 0.6, bounce: 0, delay: 1 }}
+        >
           Hi, I am <span>Nikhil</span>, a software developer based in India. I
           am a passionate software developer looking to create some interesting
           stuff and meeting awesome people while I do that. Feel free to connect
           with me.
-        </p>
+        </motion.p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
