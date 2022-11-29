65070194747b1c350f9a9efbf9dc74c66f309a30
itsmenikhill <44477975+itsmenikhill@users.noreply.github.com>
2022-11-29 20:42:25 +0000

    Deploying to gh-pages from @ itsmenikhill/nikhilsharma.github.io@f61a0f79ce07b14eac1790d9117fea149a2b9e41 🚀

---
+++ _next/static/chunks/pages/components/Banner-7678fb3f492c38ed.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[748],{3294:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Banner",function(){return u(8351)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3294)}),_N_E=n.O()}]);
